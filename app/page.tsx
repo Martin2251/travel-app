3cd222f3b4fbeb17b750f76dc2dfd5bdc8db1c16
Martin Lowe <martinlowe673@gmail.com>
2023-10-14 13:08:06 +0100

    added new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Campt from '@/components/Campt'
+import Features from '@/components/Features'
+import GetApp from '@/components/GetApp'
+import Guide from '@/components/Guide'
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
   <>
+  <Hero  />
+  <Campt  />
+  <Guide  />
+  <Features  />
+  <GetApp  />
   
   </>
   )
