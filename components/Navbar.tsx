7bd51d45f9bad025d8d4469e563bcf129e7844ff
Martin Lowe <martinlowe673@gmail.com>
2023-10-14 11:24:09 +0100

    added constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link"
+import Image from "next/image"
 
 
 const Navbar = () => {
   return (
-  <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+  <nav className=" flexBetween max-container padding-container relative z-30 py-5">
     <Link href="/">
-        
+        <Image alt="logo" src="/hilink-logo.svg" width={74} height={29} />
+        <ul className="hidden h-full gap-12 lg:flex">
+            
+        </ul>
     </Link>
   </nav>
   )
