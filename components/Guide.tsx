b767964d10542a186dc71ac41821bd896080f6fb
Martin Lowe <martinlowe673@gmail.com>
2023-10-15 10:25:34 +0100

    start of guide

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className='flexCenter flex-col'>
+        <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+            <Image src="/camp.svg" alt="camp" width={50} height={50}  />
+           <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'> we are here for you</p>
+           <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
+            <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'> Guide you to east path</h2>
+            <p></p>
+           </div>
+        </div>
+    </section>
   )
 }
 
